f441070ca3695c2ba090466093b33bdcd373b249
Park YeJun <yejun1348@gmail.com>
2026-03-31 17:48:59 +0900

    modified

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function sayHello() {
   alert("Hello!");
 }
+
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.style.boxShadow = '0 10px 30px rgba(0,0,0,0.3)';
+    navbar.style.padding = '15px 10%';
+  } else {
+    navbar.style.boxShadow = 'none';
+    navbar.style.padding = '20px 10%';
+  }
+});
